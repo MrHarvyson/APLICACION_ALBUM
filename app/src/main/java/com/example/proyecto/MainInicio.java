f7f0757213bd47413c8e42c143b462bef7275e7e
MrHarvyson <josecmoreno3d@gmail.com>
2023-01-07 21:36:20 +0100

    ultimos cambios antes de fragment2

--- app/src/main/java/com/example/proyecto/MainInicio.java
+++ app/src/main/java/com/example/proyecto/MainInicio.java
@@ -8,22 +8,16 @@ import androidx.fragment.app.FragmentTransaction;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Pair;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.proyecto.databinding.ActivityMainBinding;
 import com.example.proyecto.databinding.ActivityMainInicioBinding;
-import com.example.proyecto.db.Db;
+import com.example.proyecto.fragment.ListaFragment;
+import com.example.proyecto.fragment.ModificarFragment;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 public class MainInicio extends AppCompatActivity {
 
