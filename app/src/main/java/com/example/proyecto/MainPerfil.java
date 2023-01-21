1d983251141c99979c5f91639a726f4724649439
MrHarvyson <josecmoreno3d@gmail.com>
2023-01-21 01:05:29 +0100

    pagina usuario

--- app/src/main/java/com/example/proyecto/MainPerfil.java
+++ app/src/main/java/com/example/proyecto/MainPerfil.java
@@ -2,10 +2,26 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.proyecto.db.Db;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.squareup.picasso.Picasso;
 
 public class MainPerfil extends AppCompatActivity {
-
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient gsc;
+    private ImageView imgFoto;
+    private TextView textUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +30,28 @@ public class MainPerfil extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main_perfil);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        imgFoto = findViewById(R.id.imgFoto);
+        textUsuario = findViewById(R.id.textUsuario);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+
+        Uri foto = null;
+        if (acct != null) {
+            String nombre = acct.getGivenName();
+            foto = acct.getPhotoUrl();
+            Usuario.crearusuario(nombre);
+            Picasso.get().load(foto).into(imgFoto);
+        }else {
+            imgFoto.setImageBitmap(Db.seleccionarUsuario(MainPerfil.this, Usuario.getNombre()));
+        }
+
+        textUsuario.setText(Usuario.getNombre());
     }
+
+    public void back(View view){
+        onBackPressed();
+    }
+
 }
